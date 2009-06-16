87ee555aaddcf8b1a49b9dea762ac5ce42469a0f
Ryusuke Konishi <konishi.ryusuke@lab.ntt.co.jp>
2009-06-16 12:41:37 +0900

    nilfs2: remove meaningless EBUSY case from nilfs_get_sb function
    The following EBUSY case in nilfs_get_sb() is meaningless.  Indeed,
    this error code is never returned to the caller.
    
        if (!s->s_root) {
              ...
        } else if (!(s->s_flags & MS_RDONLY)) {
            err = -EBUSY;
        }
    
    This simply removes the else case.
    
    Signed-off-by: Ryusuke Konishi <konishi.ryusuke@lab.ntt.co.jp>

--- fs/super.c
+++ fs/super.c
@@ -1253,8 +1253,6 @@ nilfs_get_sb(struct file_system_type *fs_type, int flags,
 		}
 #endif
 		need_to_close = 0;
-	} else if (!(s->s_flags & MS_RDONLY)) {
-		err = -EBUSY;
 	}
 
 	nilfs_unlock_bdev(sd.bdev);
